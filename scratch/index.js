8bb554792a9683026d2301ccb7413958011c611b
suyu0925 <suyu0925@gmail.com>
2019-03-07 08:07:03 +0800

    更新一波scratch

--- scratch/index.js
+++ scratch/index.js
@@ -1,7 +1,15 @@
 const Koa = require('koa')
 const koaStatic = require('koa-static')
+const koaStaticCache = require('koa-static-cache')
+const compress = require('koa-compress')
 
 const app = new Koa()
-app.use(koaStatic(`${__dirname}/public`))
+app.use(compress())
+app.use(koaStaticCache(`${__dirname}/public`, {
+  maxAge: 60 * 60 * 1000
+}))
+app.use(koaStatic(`${__dirname}/public`, {
+  maxage: 60 * 60 * 1000
+}))
 
 app.listen(8064)
