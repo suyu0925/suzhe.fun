9ce623fd20955f2d18f87cb7a205b5c4ed9eed35
suyu0925 <suyu0925@gmail.com>
2019-09-02 17:24:24 +0800

    把配置存在cookie里

--- arithmetic/src/App.vue
+++ arithmetic/src/App.vue
@@ -19,6 +19,7 @@ import Console from '@/components/console.vue'
 import Paper from '@/components/paper.vue'
 import { Printd } from 'printd'
 import cssText from '@/printCss'
+import * as Cookies from 'js-cookie'
 
 export default {
   components: {
@@ -34,6 +35,30 @@ export default {
         title: '练习题'
       },
       loading: false
+    }
+  },
+  created() {
+    // load config from cookie
+    // console.log('load cookie')
+    const grade = Cookies.get('grade')
+    // console.log('grade', grade)
+    if (grade) {
+      this.config.grade = parseInt(grade, 10)
+    }
+    const type = Cookies.get('type')
+    // console.log('type', type)
+    if (type) {
+      this.config.type = type
+    }
+  },
+  watch: {
+    'config.grade': function(grade) {
+      // console.log('save grade to cookie', grade)
+      Cookies.set('grade', grade)
+    },
+    'config.type': function(type) {
+      // console.log('save type to cookie', type)
+      Cookies.set('type', type)
     }
   },
   methods: {
